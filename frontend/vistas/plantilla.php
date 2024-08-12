960bba8c58e7589734cc7e056f5bf2ea1ccbb057
javierBorjas <cristman11@gmail.com>
2024-08-12 01:24:41 -0300

    Maquetando el Cabezote con html

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Duis at consequat quam, vitae luctus odio. In porta sapien at justo congue imperdiet. Fusce mattis nisi eu massa dignissim, et ultricies sem facilisis.">
+    <meta name="keyword" content="Curabitur tempor scelerisque turpis sit amet scelerisque. Nullam felis turpis, lacinia vel odio sed, mollis posuere ante">
+
     <title>Tienda Virtual</title>
+
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
+
+
 </head>
 <body>
-    <h1>Titulo</h1>
+
+<?php
+
+/*=====================================
+CABEZOTE
+======================================*/
+
+include "modulos/cabezote.php"
+
+?>
+
 </body>
 </html>
