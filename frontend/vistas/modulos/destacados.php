2e87415584bf2d0ca79efffad6c2018ab25612bc
javierBorjas <cristman11@gmail.com>
2024-08-20 23:25:44 -0300

    Maquetando la vitrina de productos mas vendidos

--- frontend/vistas/modulos/destacados.php
+++ frontend/vistas/modulos/destacados.php
@@ -103,6 +103,8 @@ VITRINA DE PRODUCTOS GRATIS
 
             </div>
 
+            <div class="clearfix"></div>
+
             <hr>
 
         </div>
@@ -137,7 +139,7 @@ VITRINA DE PRODUCTOS GRATIS
 
                         <a href="#" class="pixelProducto">
 
-                            Collar de diamantes<br>
+                            Collar de diamantes<br><br>
 
                         </a>
 
@@ -205,7 +207,7 @@ VITRINA DE PRODUCTOS GRATIS
 
                         <a href="#" class="pixelProducto">
 
-                            Bolso deportivo gris<br>
+                            Bolso deportivo gris<br><br>
 
                         </a>
 
@@ -273,7 +275,7 @@ VITRINA DE PRODUCTOS GRATIS
 
                         <a href="#" class="pixelProducto">
 
-                            Bolso militar<br>
+                            Bolso militar<br><br>
 
                         </a>
 
@@ -343,6 +345,8 @@ VITRINA DE PRODUCTOS GRATIS
 
                             Pulsera de diamantes<br>
 
+                            <span class="label label-warning fontSize">Nuevo</span>
+
                         </a>
 
                     </small>
@@ -391,3 +395,423 @@ VITRINA DE PRODUCTOS GRATIS
 
 </div>
 
+<!--=====================================
+BARRA PRODUCTOS MÁS VENDIDOS
+======================================-->
+
+<div class="container-fluid well well-sm barraProductos">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-xs-12 organizarProductos">
+
+                <div class="btn-group pull-right">
+
+                    <button type="button" class="btn btn-default btnGrid" id="btnGrid1">
+
+                        <i class="fa fa-th" aria-hidden="true"></i>
+
+                        <span class="col-xs-0 pull-right"> GRID</span>
+
+                    </button>
+
+                    <button type="button" class="btn btn-default btnList" id="btnList1">
+
+                        <i class="fa fa-list" aria-hidden="true"></i>
+
+                        <span class="col-xs-0 pull-right"> LIST</span>
+
+                    </button>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!--=====================================
+VITRINA DE PRODUCTOS MÁS VENDIDOS
+======================================-->
+
+<div class="container-fluid productos">
+
+    <div class="container">
+
+        <div class="row">
+
+            <!--=====================================
+            BARRA TÍTULO
+            ======================================-->
+
+            <div class="col-xs-12 tituloDestacado">
+
+                <!--=======================================================-->
+
+                <div class="col-sm-6 col-xs-12">
+
+                    <h1><small>LO MÁS VENDIDO </small></h1>
+
+                </div>
+
+                <!--=======================================================-->
+
+                <div class="col-sm-6 col-xs-12">
+
+                    <a href="lo-mas-vendido">
+
+                        <button class="btn btn-default backColor pull-right">
+
+                            VER MÁS <span class="fa fa-chevron-right"></span>
+
+                        </button>
+
+                    </a>
+
+                </div>
+
+                <!--=======================================================-->
+
+            </div>
+
+            <div class="clearfix"></div>
+
+            <hr>
+
+        </div>
+
+        <!--=====================================
+        VITRINA DE PRODUCTOS EN CUADRÍCULA
+        ======================================-->
+
+        <ul class="gri1">
+
+            <!-- Producto 1 -->
+
+            <li class="col-md-3 col-sm-6 col-xs-12">
+
+                <!--=======================================================-->
+
+                <figure>
+
+                    <a href="#" class="pixelProducto">
+
+                        <img src="http://localhost/ecommerce-php/backend/vistas/img/productos/ropa/ropa03.jpg" class="img-responsive">
+
+                    </a>
+
+                </figure>
+
+                <!--=======================================================-->
+
+                <h4>
+
+                    <small>
+
+                        <a href="#" class="pixelProducto">
+
+                            Falda de Flores<br>
+
+                            <span class="label label-warning fontSize">Nuevo</span>
+
+                            <span class="label label-warning fontSize">40% off</span>
+
+                        </a>
+
+                    </small>
+
+                </h4>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 precio">
+
+                    <h2>
+
+                        <small>
+
+                            <strong class="oferta">USD $29</strong>
+
+                        </small>
+
+                        <small>$11</small>
+
+                    </h2>
+
+                </div>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 enlaces">
+
+                    <div class="btn-group pull-right">
+
+                        <button type="button" class="btn btn-default btn-xs deseos" idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+
+                            <button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto">
+
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+            <!-- Producto 2 -->
+
+            <li class="col-md-3 col-sm-6 col-xs-12">
+
+                <!--=======================================================-->
+
+                <figure>
+
+                    <a href="#" class="pixelProducto">
+
+                        <img src="http://localhost/ecommerce-php/backend/vistas/img/productos/ropa/ropa04.jpg" class="img-responsive">
+
+                    </a>
+
+                </figure>
+
+                <!--=======================================================-->
+
+                <h4>
+
+                    <small>
+
+                        <a href="#" class="pixelProducto">
+
+                            Vestido Jean<br>
+
+                            <span class="label label-warning fontSize">40% off</span>
+
+                        </a>
+
+                    </small>
+
+                </h4>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 precio">
+
+                    <h2>
+
+                        <small>
+
+                            <strong class="oferta">USD $29</strong>
+
+                        </small>
+
+                        <small>$11</small>
+
+                    </h2>
+
+                </div>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 enlaces">
+
+                    <div class="btn-group pull-right">
+
+                        <button type="button" class="btn btn-default btn-xs deseos" idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+
+                            <button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto">
+
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+            <!-- Producto 3 -->
+
+            <li class="col-md-3 col-sm-6 col-xs-12">
+
+                <!--=======================================================-->
+
+                <figure>
+
+                    <a href="#" class="pixelProducto">
+
+                        <img src="http://localhost/ecommerce-php/backend/vistas/img/productos/ropa/ropa02.jpg" class="img-responsive">
+
+                    </a>
+
+                </figure>
+
+                <!--=======================================================-->
+
+                <h4>
+
+                    <small>
+
+                        <a href="#" class="pixelProducto">
+
+                            Vestido Clásico<br>
+
+                            <span class="label label-warning fontSize">40% off</span>
+
+                        </a>
+
+                    </small>
+
+                </h4>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 precio">
+
+                    <h2>
+
+                        <small>
+
+                            <strong class="oferta">USD $29</strong>
+
+                        </small>
+
+                        <small>$11</small>
+
+                    </h2>
+
+                </div>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 enlaces">
+
+                    <div class="btn-group pull-right">
+
+                        <button type="button" class="btn btn-default btn-xs deseos" idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+
+                            <button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto">
+
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+            <!-- Producto 4 -->
+
+            <li class="col-md-3 col-sm-6 col-xs-12">
+
+                <!--=======================================================-->
+
+                <figure>
+
+                    <a href="#" class="pixelProducto">
+
+                        <img src="http://localhost/ecommerce-php/backend/vistas/img/productos/ropa/ropa06.jpg" class="img-responsive">
+
+                    </a>
+
+                </figure>
+
+                <!--=======================================================-->
+
+                <h4>
+
+                    <small>
+
+                        <a href="#" class="pixelProducto">
+
+                            Top Dama
+
+                            <br>
+                            <br>
+
+                        </a>
+
+                    </small>
+
+                </h4>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 precio">
+
+                    <h2>
+
+                        <small>USD $29</small>
+
+                    </h2>
+
+                </div>
+
+                <!--=======================================================-->
+
+                <div class="col-xs-6 enlaces">
+
+                    <div class="btn-group pull-right">
+
+                        <button type="button" class="btn btn-default btn-xs deseos" idProducto="470" data-toggle="tooltip" title="Agregar a mi lista de deseos">
+
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+
+                        </button>
+
+                        <a href="#" class="pixelProducto">
+
+                            <button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto">
+
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+
+                            </button>
+
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+
+        </ul>
+
+    </div>
+
+</div>
